dabb7352bcd8420d4662db51fcc9145db799e9a0
Eunyeol <ney9083@gmail.com>
2021-10-25 21:10:20 +0900

    [Redux middleware] middleware 만들어보기

--- learn-redux-middleware-3/src/index.js
+++ learn-redux-middleware-3/src/index.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { createStore } from 'redux';
-import rootReducer from './modules';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { applyMiddleware, createStore } from "redux";
+import rootReducer from "./modules";
+import { Provider } from "react-redux";
+import loggerMiddleware from "./lib/loggerMiddleware";
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
